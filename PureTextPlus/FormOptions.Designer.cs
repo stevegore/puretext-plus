093aef10f834e69c21c2f3ae16a8a39878ee3107
mellowaredev <mellowaredev@gmail.com>
2011-05-26 18:24:24 +0000

    updated license text to Apache 2.0

--- PureTextPlus/FormOptions.Designer.cs
+++ PureTextPlus/FormOptions.Designer.cs
@@ -1,11 +1,26 @@
 ﻿/*
- * Created by SharpDevelop.
- * User: lefkoe
- * Date: 5/26/2011
- * Time: 12:03 AM
- * 
- * To change this template use Tools | Options | Coding | Edit Standard Headers.
- */
+    PureText+ - http://code.google.com/p/puretext-plus/
+    
+    Copyright (C) 2003 Steve P. Miller, http://www.stevemiller.net/puretext/
+    Copyright (C) 2011 Melloware, http://www.melloware.com
+    
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+    
+    The idea of the Original PureText Code is Copyright (C) 2003 Steve P. Miller
+    
+    NO code was taken from the original project this was rewritten from scratch
+    from just the idea of Puretext.
+*/
 namespace PureTextPlus
 {
 	partial class FormOptions
